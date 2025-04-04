c0bd803171edb690227bef739fd204e0fea7b54c
tim <tim@brightsidedeveloper.com>
2025-04-04 18:17:44 -0500

    Good Start

--- src/client/MCPClient.ts
+++ src/client/MCPClient.ts
@@ -1,12 +1,46 @@
+import { Tool } from '@anthropic-ai/sdk/resources/messages/messages.mjs'
+import { Client } from '@modelcontextprotocol/sdk/client/index.js'
+import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
-class MCPClient {
-  constructor() {}
+interface Config {
+  mcpServers: Record<string, MCPServer>
+}
 
-  helloWorld() {
-    console.log('Hello World!')
+interface MCPServer {
+  command: string
+  args: string[]
+}
+
+class MCPClient {
+  private mcp: Client
+  private transport: StdioClientTransport | null = null
+  private tools: Tool[] = []
+  private config: Config
+
+  constructor() {
+    this.mcp = new Client({ name: 'mcp-client', version: '1.0.0' })
+    this.config = this.getConfig()
+    this.connectToServers()
+  }
+
+  private getConfig() {
+    return {
+      mcpServers: {
+        email: {
+          command: 'node',
+          args: ['/Users/timvanlerberg/Desktop/mcp-2/build/index.js'],
+        },
+      },
+    }
+  }
+
+  private async connectToServers() {}
+
+  public async helloWorld() {
+    console.log('Hello, world!')
   }
 }
 
